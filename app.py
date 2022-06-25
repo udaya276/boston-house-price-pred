38242c6c0cf1a0b8f0777d43058ed4226f38d14b
udaya276 <udaya276@gmail.com>
2022-06-25 21:22:05 +0530

    app.py changed

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def PricePrediction():
     return render_template('home.html', output1=output, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'],query6 = request.form['query6'], query7 = request.form['query7'],query8 = request.form['query8'],query9 = request.form['query9'],query10 = request.form['query10'],query11 = request.form['query11'], query12 = request.form['query12'],query13 = request.form['query13'])
 
 
-
+"""
 @app.route("/via_postman", methods=["post"])
 def boston_price_prediction():
     if (request.method=='POST'):
@@ -60,6 +60,6 @@ def boston_price_prediction():
         price = model.predict(new_df)
         return(jsonify(float(price)))
 
-
+"""
 
 app.run()
