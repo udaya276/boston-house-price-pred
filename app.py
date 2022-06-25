1396482bf7d16d35ff47841077dfa8da588b0d29
udaya276 <udaya276@gmail.com>
2022-06-25 23:37:57 +0530

    app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ q = ""
 def loadPage():
 	return render_template('home.html', query="")
 
-@app.route("/predict", methods=['POST'])
+@app.route("/", methods=['POST'])
 def predict():
     query1 = float(request.form['query1'])
     query3 = float(request.form['query3'])
@@ -29,10 +29,10 @@ def predict():
     new_df = pd.DataFrame(data)
     model = pickle.load(open("model1.sav", "rb"))
     price = float(model.predict(new_df))
-    output="Prediction price is: {}".format(price)
+    #output="Prediction price is: {}".format(price)
 
 
-    return render_template('home.html', output1=output, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'],query6 = request.form['query6'], query7 = request.form['query7'],query8 = request.form['query8'],query9 = request.form['query9'],query10 = request.form['query10'],query11 = request.form['query11'], query12 = request.form['query12'],query13 = request.form['query13'])
+    return render_template('home.html', output1=price, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'],query6 = request.form['query6'], query7 = request.form['query7'],query8 = request.form['query8'],query9 = request.form['query9'],query10 = request.form['query10'],query11 = request.form['query11'], query12 = request.form['query12'],query13 = request.form['query13'])
 
 
 
