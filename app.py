98e3b2a4fdaba379fc1468454bacc2db6c2f4b23
udaya276 <udaya276@gmail.com>
2022-06-26 00:56:15 +0530

    app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def loadPage():
-	return render_template('home.html', query="")
+	return render_template('home.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
